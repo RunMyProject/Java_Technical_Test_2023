ee85f0366b56a295ecdafc68cbfbb0b5338e913f
RunMyProject <esabatini74@gmail.com>
2023-01-13 18:43:12 +0100

    First scratch to show version in java 17

---
+++ Technical_Test_2023/src/main/java/mypack/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package mypack;
+
+/**
+ * @author edoardo
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Since Java 9
+		//
+		Runtime.Version version = Runtime.version();
+		
+		System.out.println("Current Java version: " + version);
+		System.out.println("------------------------------------------");		
+	}
+	
+}
---
+++ Technical_Test_2023/src/main/java/mypack/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author edoardo
+ *
+ */
+package mypack;
